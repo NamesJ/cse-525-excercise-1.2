43b0a2fdc42cb6debfd1076aca82932c30864ca6
Jacob Sanders <sanders.j4@gmail.com>
2023-02-17 07:57:58 -0500

    Add files via upload

--- include/peripherals/uart.h
+++ include/peripherals/uart.h
@@ -2,16 +2,12 @@
 #define _P_UART_H
 
 #include "peripherals/base.h"
-#define PL011_UART_BASE	0x7E201000
-#define	UART_DR			(PL011_UART_BASE+0x0)	/* Data register */
-#define UART_FR			(PL011_UART_BASE+0x18)	/* Flag register */
-#define UART_IBRD		(PL011_UART_BASE+0x24)	/* Integer Baud rate divisor */
-#define UART_FBRD		(PL011_UART_BASE+0x28)	/* Fractional Baud rate divisor */
-#define UART_LCRH		(PL011_UART_BASE+0x2C)	/* Line Control register */
-#define UART_CR			(PL011_UART_BASE+0x30)	/* Control register */
-#define UART_IFLS		(PL011_UART_BASE+0x34)	/* Interupt FIFO Level Select register */
-#define UART_IMSC		(PL011_UART_BASE+0x38)	/* Interupt Mask Set Clear register */
-#define UART_RIS		(PL011_UART_BASE+0x3C)	/* Raw Interupt Status Register */
-#define UART_ICR		(PL011_UART_BASE+0x44)	/* Interupt Clear Register */
+
+#define	UART_DR			(PBASE+0x201000)	/* Data register */
+#define UART_FR			(PBASE+0x201018)	/* Flag register */
+#define UART_IBRD		(PBASE+0x201024)		/* Integer Baud rate divisor */
+#define UART_FBRD		(PBASE+0x201028)		/* Fractional Baud rate divisor */
+#define UART_LCRH		(PBASE+0x20102C)		/* Line Control register */
+#define UART_CR			(PBASE+0x201030)				/* Control register */
 
 #endif /* _P_UART_H */
--- src/uart.c
+++ src/uart.c
@@ -4,24 +4,20 @@
 
 void uart_send ( char c )
 {
-	while(1) {
-		//if(get32(AUX_MU_LSR_REG)&0x20) 
-		if(get32(UART_FR&0x80))
-			break;
+	/* Transmit FIFO is full */
+	while(get32(UART_FR) & (1<<5)) {
+		;
 	}
-	//put32(AUX_MU_IO_REG,c);
-	put32(UART_DR&0x000F,c);
+	put32(UART_DR,c);
 }
 
 char uart_recv ( void )
 {
-	while(1) {
-		//if(get32(AUX_MU_LSR_REG)&0x01) 
-		if(get32(UART_FR&0x20))
-			break;
+	/* Receive FIFO is empty */ 
+	while(get32(UART_FR) & (1<<4)) {
+		;
 	}
-	//return(get32(AUX_MU_IO_REG)&0xFF);
-	get32(UART_DR&0x000F);
+	return get32(UART_DR) & 0xFF;
 }
 
 void uart_send_string(char* str)
@@ -33,42 +29,24 @@ void uart_send_string(char* str)
 
 void uart_init ( void )
 {
-	/*
 	unsigned int selector;
+	
 	selector = get32(GPFSEL1);
 	selector &= ~(7<<12);                   // clean gpio14
-	selector |= 2<<12;                      // set alt5 for gpio14
+	selector = selector | (4<<12);      	// set alt1 for gpio14 
 	selector &= ~(7<<15);                   // clean gpio15
-	selector |= 2<<15;                      // set alt5 for gpio15
+	selector = selector | (4<<15);      	// set alt1 for gpio15
 	put32(GPFSEL1,selector);
-	*/
+
 	put32(GPPUD,0);
 	delay(150);
 	put32(GPPUDCLK0,(1<<14)|(1<<15));
 	delay(150);
 	put32(GPPUDCLK0,0);
-	/*
-	put32(AUX_ENABLES,1);                   //Enable mini uart (this also enables access to its registers)
-	put32(AUX_MU_CNTL_REG,0);               //Disable auto flow control and disable receiver and transmitter (for now)
-	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
-	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
-	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,3254);             //Set baud rate to 9600
-
-	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
-	*/
-	// Disable the UART
-	put32(UART_CR,0);
-	// Wait for the end of transmission or reception of the current character
-	//?
-	// Flush the transmit FIFO by setting the FEN bit to 0 in Line Control Register, UART_LCRH
-	put32(UART_LCRH,0x60);
-	// Reprogram the Control Register, UART_CR
-	put32(UART_CR,0x4300);
-	// Enable the UART
-	put32(UART_CR,0x4301);
 	
-	// Set the baud rate
-	put32(UART_IBRD, 30&0xFFFF);
-	put32(UART_FBRD, 0&0x3F);
+	put32(UART_CR,0);				// Disable the UART
+	put32(UART_IBRD,26);			// Set baud rate 115200
+	put32(UART_FBRD,3);				// (cont.)
+	put32(UART_LCRH,(1<<4)|(3<<5));	// Enable FIFO (8 bits)
+	put32(UART_CR, (1 | (3<<8)));   // Enable UART for RX and TX
 }
